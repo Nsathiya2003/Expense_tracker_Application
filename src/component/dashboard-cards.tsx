789fc13dd8eeb84d3b06ef9a03347dc0800a34af
sathiya <nsathiya757@gmail.com>
2025-11-02 20:21:34 +0530

    Counts cards showed in dashboard

---
+++ src/component/dashboard-cards.tsx
@@ -0,0 +1,45 @@
+import { CardItems } from "../data/dashboard-card-items";
+
+export default function Cards() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 w-full lg:w-[1160px] px-4">
+      {CardItems.map((item, index) => {
+        const Icon = item.icon;
+        return (
+          <div
+            key={index}
+            className="bg-[#2E362E] text-white p-6 rounded-xl shadow-md hover:shadow-lg hover:scale-[1.02] transition-all duration-300"
+          >
+            <div className="flex justify-between items-center mb-4">
+              {/* Text + Label Section */}
+              <div>
+                <h2 className="text-lg font-semibold text-[#00C8DC]">
+                  {item.label}
+                </h2>
+                <p className="text-gray-400 text-sm">{item.description}</p>
+              </div>
+
+              {/* Icon Section */}
+              <div className="p-3 bg-[#00C8DC]/20 rounded-full flex justify-center items-center">
+                <Icon className="w-8 h-8 text-[#00C8DC]" />
+              </div>
+            </div>
+
+            {/* Numbers Section */}
+            <div className="flex justify-between items-end mt-4">
+              <div>
+                <p className="text-gray-400 text-sm">Records (30 days)</p>
+                <h3 className="text-xl font-bold">{item.totalRecords}</h3>
+              </div>
+
+              <div className="text-right">
+                <p className="text-gray-400 text-sm">Total Amount</p>
+                <h3 className="text-xl font-bold text-[#00C8DC]">₹{item.totalAmount}</h3>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/data/dashboard-card-items.ts
@@ -0,0 +1,26 @@
+import { GiReceiveMoney } from "react-icons/gi";
+
+export const CardItems = [
+    {
+        label:'Total Expense',
+        totalRecords:12,
+        totalAmount:6000,
+        icon:GiReceiveMoney,
+        description:'your expense...'
+    },
+    {
+        label:'Total Income',
+        totalRecords:15,
+        totalAmount:7000,
+        icon:GiReceiveMoney,
+        description:'your expense...'
+    },
+     {
+        label:'Total Budgets',
+        totalRecords:10,
+        totalAmount:10000,
+        icon:GiReceiveMoney,
+        description:'your expense...'
+    },
+
+]

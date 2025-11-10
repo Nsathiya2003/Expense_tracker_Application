5e20cdd7309df6ee1b91e9b7a091aa9c5cc4894c
sathiya <nsathiya757@gmail.com>
2025-11-10 22:21:57 +0530

    Query client setup initialized

---
+++ src/api/apiClient.tsx
@@ -0,0 +1,46 @@
+import { QueryClient } from "@tanstack/react-query";
+import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+
+// const navigate = useNavigate();
+
+//1.Create a query client for implement tanstack-query
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 100 * 60 * 5,
+    },
+  },
+});
+
+//2.Interceptors for send tokens..
+export const apiClient = axios.create({
+  baseURL: import.meta.env.BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+//3.handling the global response handling
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.warn("Unauthorized -redirect to login....");
+    }
+    return Promise.reject(error);
+  }
+);

c7ade926ed994978eedff2f2ea05cfbca8cbf562
sathiya <nsathiya757@gmail.com>
2025-11-20 22:01:42 +0530

    query Client implemented for to fetch data's

--- src/api/income/income-hooks.tsx
+++ src/api/income/income-hooks.tsx
@@ -1,13 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { incomeApi, type CreateIncomePayload } from "./incomeApi";
 import { toast } from "react-toastify";
 
-export const useCreateIncome = () => {
+export const useCreateIncome = (resetForm: () => void) => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (body: CreateIncomePayload) => incomeApi.createIncome(body),
 
     onSuccess: (data) => {
       toast.success(`${data?.message}` || "your income was added");
+      setTimeout(() => {
+        queryClient.invalidateQueries({ queryKey: ["income"] });
+      }, 3000);
+      resetForm();
     },
 
     onError: (error) => {
@@ -15,5 +20,14 @@ export const useCreateIncome = () => {
         `${error?.message}` || "something went wrong add your income"
       );
     },
+  });
+};
+
+export const useGetIncome = () => {
+  return useQuery({
+    queryKey: ["income"],
+    queryFn: () => incomeApi.getIncome(),
+    staleTime: 1000 * 60 * 5,
+    refetchOnWindowFocus: false,
   });
 };

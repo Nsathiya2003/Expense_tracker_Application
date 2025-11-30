621abb7a36a549237fc82d65352cde11fd220782
sathiya <nsathiya757@gmail.com>
2025-11-30 14:52:52 +0530

    Edit function handled in main view goal

--- src/pages/goal/viewGoal.tsx
+++ src/pages/goal/viewGoal.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import AddGoal from "./AddGoal";
 import GoalTable from "./GoalTable";
 
 export default function ViewGoal() {
+  // const [openAdd, setOpenAdd] = useState(false);
+  const [editingId, setEditingId] = useState<string | null>(null);
+
   return (
     <div className="flex flex-col gap-4 p-4">
-      <AddGoal />
-      <GoalTable />
+      <AddGoal editingId={editingId} />
+
+      <GoalTable
+        onEdit={(id) => {
+          setEditingId(id);
+          // setOpenAdd(true);
+        }}
+      />
     </div>
   );
 }

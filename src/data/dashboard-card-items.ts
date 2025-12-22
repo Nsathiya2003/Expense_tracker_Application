9fd7fcfd8cfa0df5630ff43924493e336f092008
sathiya <nsathiya757@gmail.com>
2025-12-22 23:19:24 +0530

    Dashboard card items api was integrated

--- src/data/dashboard-card-items.ts
+++ src/data/dashboard-card-items.ts
@@ -1,29 +1,25 @@
-import { MdPayment, MdSavings } from "react-icons/md";
-import { FaMoneyBillWave } from "react-icons/fa";
+import { FaMoneyBillWave, FaWallet, FaPiggyBank } from "react-icons/fa";
 
 export const CardItems = [
   {
-    label: "Total Income",
-    totalRecords: 15,
-    totalAmount: 7000,
-    icon: MdPayment,
-    color: "rgba(34, 197, 94, 1)", // green
-    description: "Your income summary...",
-  },
-  {
-    label: "Total Expense",
-    totalRecords: 12,
-    totalAmount: 6000,
-    icon: MdSavings,
-    color: "rgba(240, 152, 152, 1)", // red
-    description: "Your expense details...",
-  },
-  {
-    label: "Total Budgets",
-    totalRecords: 10,
-    totalAmount: 10000,
+    key: "income",
+    label: "Income",
+    description: "Total income received",
     icon: FaMoneyBillWave,
-    color: "rgba(59, 130, 246, 1)", // blue
-    description: "Your budget overview...",
+    color: "#00C8DC",
+  },
+  {
+    key: "expense",
+    label: "Expense",
+    description: "Total expenses spent",
+    icon: FaWallet,
+    color: "#FF6B6B",
+  },
+  {
+    key: "budget",
+    label: "Budget",
+    description: "Total planned budget",
+    icon: FaPiggyBank,
+    color: "#4ADE80",
   },
 ];

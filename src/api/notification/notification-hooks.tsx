7e84d24a9702c56f01a82df1acf6dd1ce62d575d
sathiya <nsathiya757@gmail.com>
2025-12-25 22:27:11 +0530

    notification hooks was created for mark as read and delete

--- src/api/notification/notification-hooks.tsx
+++ src/api/notification/notification-hooks.tsx
@@ -1,9 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import {
   notificationApi,
   type filterNotificationPayload,
 } from "./notificationApi";
 import type { NotificationTypeResponse } from "../../types/response-types";
+
+export interface markNotificationAsReadPayload {
+  id: string;
+  read: boolean;
+}
 
 export const useNotificationFilter = (filters: filterNotificationPayload) => {
   return useQuery<NotificationTypeResponse>({
@@ -11,5 +16,21 @@ export const useNotificationFilter = (filters: filterNotificationPayload) => {
     queryFn: () => notificationApi.useNotificationFilter(filters),
     staleTime: 1000 * 60,
     refetchOnWindowFocus: false,
+  });
+};
+
+export const useMarkNotificationAsRead = () => {
+  return useMutation({
+    mutationFn: async (ids: string[]) => {
+      await notificationApi.markAsRead({ ids });
+    },
+  });
+};
+
+export const useDeleteNotifications = () => {
+  return useMutation({
+    mutationFn: async (ids: string[]) => {
+      await notificationApi.deleteNotifications({ ids });
+    },
   });
 };

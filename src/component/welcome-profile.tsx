f0e98629a4d661b198f81d36b19e98a762f1a38e
sathiya <nsathiya757@gmail.com>
2025-12-27 16:41:42 +0530

    Welcome profile designed

--- src/component/welcome-profile.tsx
+++ src/component/welcome-profile.tsx
@@ -28,30 +28,30 @@ export default function WelcomeProfileComponent() {
   }, [userData]);
   return (
     <>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-3 sm:gap-4 px-4 sm:px-0">
         {previewUrl ? (
           <img
             src={previewUrl}
             alt="profile"
-            className="w-20 h-20 rounded-full object-cover"
+            className="w-12 h-12 sm:w-20 sm:h-20 rounded-full object-cover border-2 border-[#00C8DC]"
           />
         ) : (
           <img
             src={profile}
             alt="profile"
-            className="w-20 h-20 rounded-full object-cover"
+            className="w-12 h-12 sm:w-20 sm:h-20 rounded-full object-cover"
           />
         )}
 
         <div>
-          <h1 className="text-2xl font-semibold text-white">
+          <h1 className="text-lg sm:text-2xl font-semibold text-white">
             Welcome Back,{" "}
             <span className="text-[#00C8DC]">
               {data?.username.charAt(0).toUpperCase() +
                 data?.username.slice(1) || "User"}
             </span>
           </h1>
-          <p className="text-gray-400 text-sm mt-1 italic">
+          <p className="text-md sm:text-sm text-gray-400 mt-1 italic">
             Know your spend. Own your life.
           </p>
         </div>
